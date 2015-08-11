2325d0af6fffe87c6061d236f878fbb57e41c355
lookOnTheBrightSide <nkosindlovu87@gmail.com>
2015-08-12 01:11:00 +0200

    created new module for fortune function

---
+++ express_oriely/chapter_4/app.js
@@ -0,0 +1,52 @@
+var express = require('express');
+var app = express();
+var fortune = require('./lib/fortune.js')
+app.use(express.static(__dirname + '/public'));
+var handlebars = require('express-handlebars')
+    .create({
+        defaultLayout: 'main'
+    });
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+
+
+// app.engine('handlebars', exphbs({
+//     defaultLayout: 'main'
+// }));
+// app.set('view engine', 'handlebars');
+// app.use(express.static('views'));
+// app.use(express.static('public'));
+
+
+app.set('port', process.env.PORT || 3000);
+
+
+
+app.get('/', function(req, res) {
+    res.render('home');
+});
+app.get('/about', function(req, res) {
+    res.render('about', {
+        fortune: fortune.getFortune()
+    });
+});
+// 404 catch-all handler (middleware)
+app.use(function(req, res, next) {
+    res.status(404);
+    res.render('404');
+});
+// 500 error handler (middleware)
+app.use(function(err, req, res, next) {
+    console.error(err.stack);
+    res.status(500);
+    res.render('500');
+});
+// custom 404 page
+app.use(function(req, res, next) {
+    res.type('text/plain');
+    res.status(404);
+    res.send('404 - Not Found');
+});
+app.listen(app.get('port'), function() {
+    console.log('Express on http://localhost:' + app.get('port'))
+});
