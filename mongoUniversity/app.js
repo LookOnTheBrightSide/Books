acdd597c9db7a8038199be279c1906250665585c
lookOnTheBrightSide <nkosindlovu87@gmail.com>
2015-08-17 00:28:38 +0200

    mongoUni connection callback

--- mongoUniversity/app.js
+++ mongoUniversity/app.js
@@ -14,3 +14,67 @@ MongoClient.connect('mongodb://localhost:27017/course', function(err, db) {
         db.close()
     })
 })
+//=====to array
+MongoClient.connect('mongodb://localhost:27017/course', function(err, db) {
+    if (err) {
+        console.log(err)
+    };
+    var query = {
+        name: "shaggy"
+    };
+    db.collection('grades').find(query).toArray(function(err, doc) {
+        if (err) 
+            throw err;
+        
+        console.dir(doc)
+        db.close()
+    })
+})
+
+MongoClient.connect('mongodb://localhost:27017/course', function(err, db) {
+    if (err) {
+        console.log(err)
+    };
+    var query = {
+        name: "shaggy"
+    };
+    var cursor = db.collection('grades').find(query) 
+    cursor.each(function(err,doc){
+    	if (err)
+    		throw err
+    	if (doc == null) {return db.close()};
+    	console.dir(doc.name + " is a bad ass! " + doc.song )
+    })
+    
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
