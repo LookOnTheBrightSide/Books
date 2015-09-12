123ad274cd9f7638e05c6baba97d89277567cbe9
lookOnTheBrightSide <nkosindlovu87@gmail.com>
2015-09-12 21:50:10 +0200

    notes on rest

--- node/app.js
+++ node/app.js
@@ -200,6 +200,15 @@ greetings("sam",23)
 //     or contain data (if you're POSTing or PUTing information, 
 //     that information is contained here).
 
+//Endpoints are API-defined locations where particular data are stored. 
+//  Just as you'll GET a pair of pants from PantsWorld or you'll 
+//  GET a bag of peanuts from PeanutHut, you'll GET something different 
+//  depending on the endpoint you use.
+
+//For instance, if you're using the API for a video hosting service, 
+// there might be endpoints for the most popular videos, 
+// the most recent videos, or videos belonging to a certain genre or category.
+
 
 
 var xhr = new XMLHttpRequest();
